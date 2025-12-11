1dcd8487d741b9b0506d2a55f8811e5399aaefef
Md Mushfique Raza <114329904+786Mushfique@users.noreply.github.com>
2025-12-11 12:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function mincost(arr)
 { 
 //write your code here
+  let pq = arr.sort();
+  let total = 0;
+
+    while (pq.length > 1) {
+    let x = pq.shift();
+    let y = pq.shift();
+    
+    let sum = x + y;
+    total += sum;
+
+    let i = 0;
+    while (i < pq.length && pq[i] < sum) i++;
+    pq.splice(i, 0, sum);
+  }
+
+  return total;
 // return the min cost
   
 }
